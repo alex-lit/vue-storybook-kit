2bbccac3a553cdc3242c2b0371f599caece505a5
Alexey Litovchenko <alex.lit@outlook.com>
2021-04-07 15:51:14 +0300

    chore: preparing for version update

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,4 @@
-import { vueConsoleLogMixin } from '@alexlit/vue-console-log';
+import { createMixin } from '@alexlit/vue-console-log';
 import { action } from '@storybook/addon-actions';
 import { addons } from '@storybook/addons';
 import { create } from '@storybook/theming/create';
@@ -6,6 +6,6 @@ import { html } from 'lit-html';
 
 import { mapEvents } from './map-events.util';
 
-const { $log } = vueConsoleLogMixin.methods;
+const { $log } = createMixin().methods;
 
 export { $log, action, addons, create, html, mapEvents };
