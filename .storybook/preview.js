34beb40a1682369e5356c43c955a1c89cc55280f
Alexey Litovchenko <alex.lit@outlook.com>
2021-07-29 11:14:29 +0300

    chore: fix decorator

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,5 @@
-/* eslint-disable import/extensions */
-/* eslint-disable import/no-unresolved */
-import { parameters as nuxtParameters } from '~~/.nuxt-storybook/storybook/preview.js';
+/* eslint-disable import/extensions, import/no-unresolved */
+import { parameters as nuxtParameters } from '~~/.nuxt-storybook/storybook/preview';
 
 export * from '~~/.nuxt-storybook/storybook/preview.js';
 
@@ -16,5 +15,5 @@ export const parameters = {
 
 export const decorators = [
   () =>
-    "<ui-app style='background-color: transparent;'><div style='padding: 1rem'><story></story></div></ui-app>",
+    '<ui-app style="background-color: transparent;"><div style="padding: 1rem"><story></story></div></ui-app>',
 ];
