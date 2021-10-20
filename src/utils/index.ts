d6844f3ce2452709d3cd58addc332db48a5313ea
Alexey Litovchenko <alex.lit@outlook.com>
2021-10-20 17:06:46 +0300

    chore: preparing for version update

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,7 +2,7 @@ import { createMixin } from '@alexlit/vue-console-log';
 import { action } from '@storybook/addon-actions';
 import { addons } from '@storybook/addons';
 import { create } from '@storybook/theming/create';
-import { html } from 'lit-html';
+import { html } from 'lit';
 
 import { mapEvents } from './map-events.util';
 
