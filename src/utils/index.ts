61c5a9f15be8042630f6c54bc2dbb8cd848c395a
Alexey Litovchenko <alex.lit@outlook.com>
2022-01-25 13:00:10 +0300

    refactor(deps): remove lit

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,11 +1,5 @@
-import { createMixin } from '@alexlit/vue-console-log';
-import { action } from '@storybook/addon-actions';
-import { addons } from '@storybook/addons';
-import { create } from '@storybook/theming/create';
-import { html } from 'lit';
-
-import { mapEvents } from './map-events.util';
-
-const { $log } = createMixin().methods;
-
-export { $log, action, addons, create, html, mapEvents };
+export { html } from './html.util';
+export { mapEvents } from './map-events.util';
+export { action } from '@storybook/addon-actions';
+export { addons } from '@storybook/addons';
+export { create } from '@storybook/theming/create';
